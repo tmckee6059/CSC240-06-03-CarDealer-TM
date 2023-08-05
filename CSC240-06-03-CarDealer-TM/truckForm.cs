e53c98d352a937e1324d469c5d5a4d62c714b82e
Tyler McKee <tmckee6059@my.sullivan.edu>
2023-08-05 10:56:28 -0500

    Added forms for vehicle inventory. Added control to open form when picture is clicked.

---
+++ CSC240-06-03-CarDealer-TM/truckForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC240_06_03_CarDealer_TM
+{
+    public partial class truckForm : Form
+    {
+        public truckForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
